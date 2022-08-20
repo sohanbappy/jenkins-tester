facdbfd8042cde0c68c7fcc098cdc77afadb9465
SohanBappy <sohanbappy@gmail.com>
2022-08-21 04:13:33 +0600

    StarterClass configured

--- src/main/java/com/bappy/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/bappy/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,13 +2,15 @@ package com.bappy.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
 
 
 	@GetMapping("/")
@@ -21,4 +23,8 @@ public class JenkinsDemoApplication {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenkinsDemoApplication.class);
+	}
 }
